4a82d0ae3664a54f2dd35eb8ccd7deae039f55a5
ShanitBashari <shanityerushalmi9@gmail.com>
2025-04-23 16:01:50 +0300

    add vercel configuration to fix 405 status code

--- api/server.js
+++ api/server.js
@@ -1,28 +1,23 @@
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
-const { Server } = require('socket.io');
+const {Server} = require('socket.io');
 const cors = require('cors');
 const path = require('path');
-const userRoutes = require("./routes/userRoutes");
-const songRoutes = require("./routes/songRoutes");
+const userRoutes = require("../server/routes/userRoutes");
+const songRoutes = require("../server/routes/songRoutes");
 
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 
-// Define the allowed origin(s) - Update this with your frontend URL
-const allowedOrigin = "https://ja-moveo-web-app.vercel.app";  // Replace with your actual frontend URL
-
-// Set CORS for API routes
+app.use(cors());
 const corsOptions = {
-    origin: allowedOrigin,  // Allow only your frontend URL
+    origin: "*",
     methods: ["GET", "POST"],
-    credentials: true  // Allow cookies to be sent with requests (if needed)
 };
-
-app.use(cors(corsOptions));  // Apply CORS middleware globally for all API routes
+app.use(cors(corsOptions));
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, '../client/build')));
@@ -38,7 +33,6 @@ app.get(/(.*)/, (req, res) => {
 
 const server = http.createServer(app);
 
-// MongoDB connection
 mongoose.connect("mongodb+srv://shanityerushalmi9:hCjFlo4Tpv1Pfjr4@cluster0.vzgy8hp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     .then(() => {
         console.log('Connected to MongoDB.');
@@ -47,18 +41,19 @@ mongoose.connect("mongodb+srv://shanityerushalmi9:hCjFlo4Tpv1Pfjr4@cluster0.vzgy
         console.error('Error connecting to MongoDB -', err);
     });
 
-// Socket.IO setup with CORS handling
 const io = new Server(server, {
     cors: {
-        origin: allowedOrigin,  // Allow only your frontend URL to connect
+        origin: "*",
         methods: ["GET", "POST"],
-        credentials: true,  // Allow cookies to be sent with socket requests (if needed)
-    }
+    },
 });
 
-const userSockets = new Map();  // A map to track socket IDs and roles
+const userSockets = new Map(); // A map to track socket IDs and roles .
 
-// A global variable to indicate if there is a rehearsal session going on
+/*
+ * A global variable to indicate if there is rehearsal session going on .
+ * If there is - it holds the current song , else - null .
+ */
 global.currentLiveSession = null;
 
 io.on('connection', (socket) => {
@@ -81,7 +76,7 @@ io.on('connection', (socket) => {
     socket.on('disconnect', () => {
         console.log(`User disconnected - ${socket.id}.`);
 
-        // Check if the admin has disconnected
+        // Check if the admin has disconnected .
         const role = userSockets.get(socket.id);
         userSockets.delete(socket.id);
 
